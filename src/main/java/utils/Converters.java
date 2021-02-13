d4c721c988e5710efd041fdfcd9789eef85eba2c
Pamela Brahollari <ebirdyxx@gmail.com>
2021-02-13 12:15:43 -0500

    Added comments

--- src/main/java/utils/Converters.java
+++ src/main/java/utils/Converters.java
@@ -2,10 +2,20 @@ package utils;
 
 public class Converters {
 
+    /**
+     * Converts a string to an Integer
+     * @param s
+     * @return
+     */
     public static int stringToInteger(String s){
         return Integer.parseInt(s);
     }
 
+    /**
+     * Converts a string to a double
+     * @param s
+     * @return
+     */
     public static double stringToDouble(String s){
         return Double.parseDouble(s);
     }
